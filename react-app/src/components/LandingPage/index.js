e50b79c0bebe3d05e56baf13c80c129a23595c1b
Jimmy Juarez <jimmyjuarez2021@gmail.com>
2024-03-06 20:15:02 -0600

    navigation

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-
+import React from "react";
+import "./LandingPage.css";
 const LandingPage = () => {
   return (
     <div>
-      <h1>Welcome to the Landing Page</h1>
-      <p>This is the landing page of our app.</p>
+      <h1 className="header">LarryToons Home</h1>
+      <p>Welcome to the landing page!</p>
+        <div className="image-container">
+          <img
+            className="images"
+            src="https://larrys-books.s3.amazonaws.com/books/Blue_Lightning_Issue_2.png"
+            alt="wither"
+          />
+          <img
+            className="images"
+            src="https://larrys-books.s3.amazonaws.com/books/IMG_20240306_180117.jpg"
+            alt="wither"
+          />
+          <img
+            className="images"
+            src="https://larrys-books.s3.amazonaws.com/books/Untitled249_20230902165507.png"
+            alt="wither"
+          />
+          <img
+            className="images"
+            src="https://larrys-books.s3.amazonaws.com/books/Wither_Issue_1.png"
+            alt="wither"
+          />
+        </div>
     </div>
   );
 };
